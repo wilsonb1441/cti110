b57464d03eb275575600bde4d4533b56c4b3345f
wilsonb1441 <40323020+wilsonb1441@users.noreply.github.com>
2018-06-24 02:23:12 +0000

    Grades

---
+++ P3Lab_Wilson.py
@@ -0,0 +1,23 @@
+# CTI-110
+# P3Lab
+# Brandon Wilson
+# 23 June 2018
+
+def main():
+    score = int( input("What is the test score? "))   
+    if score >= 90:
+        print("\nYour grade is: A")
+    elif score >= 80:
+        print("\nYour grade is: B")
+    elif score >= 70: 
+        print("\nYour grade is: C")
+    elif score >= 60:
+        print("\nYour grade is: D")
+    else: 
+        print("\nYour grade is: F")         
+    
+
+
+
+
+main()
